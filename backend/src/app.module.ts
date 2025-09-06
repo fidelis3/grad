6bf082bdae482e3ae9332e2be8d5dcee0e44227f
Reema_abde <Reemabdella702@gmail.com>
2025-09-06 15:53:12 +0300

    Updated backend dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,9 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Makes ConfigModule available globally
-      envFilePath: '.env', // Explicitly specify the .env file location
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URI as string), // Type assertion or use validation below
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
