bb76545186491a12fd9dace09e2e18a982063bf5
bahati <bahatifidelis2004@gmail.com>
2025-09-09 11:13:04 +0300

    Refactor start commands in package.json and add render.yaml for deployment configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,13 +16,13 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      //'https://your-frontend-domain.com',
+      'https://grad-seven.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(Number(process.env.PORT) || 5000);
 }
 bootstrap();
